657d2045c9e1a931c6d897b78a8f8354d12e2225
Twack14 <taylor_womack@yahoo.com>
2024-10-07 13:10:55 -0500

    added tests

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from textnode import TextNode
+from textnode import TextNode, text_type_bold
+
 
 def main():
-    
-    textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    textnode = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
 
     print(textnode)
-
 
 
 if __name__ == "__main__":

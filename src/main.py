50c83d3d3d8c5e84976540a1123c3f589d72d883
Twack14 <taylor_womack@yahoo.com>
2024-10-07 13:13:44 -0500

    added test cases

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from textnode import TextNode, text_type_bold
 
 
 def main():
-    textnode = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
+    node = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
 
-    print(textnode)
+    print(node)
 
 
 if __name__ == "__main__":
